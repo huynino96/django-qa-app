882158778aff90f1683cfb59e581fde6c22f37f3
huynino96 <huynino0496@gmail.com>
2021-03-05 22:56:28 +0700

    Admin Page Config

--- questionAnsProject/main/admin.py
+++ questionAnsProject/main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user')
+    search_fields = ('title', 'detail')
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer)
