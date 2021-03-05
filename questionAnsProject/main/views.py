7c7ed6e0b47137ce5ea58a119ad35a9dffd9915c
huynino96 <huynino0496@gmail.com>
2021-03-06 00:23:19 +0700

    Call API and View on Homepage

--- questionAnsProject/main/views.py
+++ questionAnsProject/main/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Question
+from django.core.paginator import Paginator
 
 # Create your views here.
+
+
 def home(request):
-    return render(request, 'index.html')
+    questions = Question.objects.all().order_by('-id')
+    paginator = Paginator(questions, 1)
+    page_num = request.GET.get('page', 1)
+    questions = paginator.page(page_num)
+    return render(request, 'index.html', {'questions': questions})
