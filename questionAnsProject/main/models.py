b1e4e7f23bcc58782a7d56504bf234df0f84812b
huynino96 <huynino0496@gmail.com>
2021-03-05 22:48:37 +0700

    Create Question and Answer Model

--- questionAnsProject/main/models.py
+++ questionAnsProject/main/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+# Question Model
+
+
+class Question(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    detail = models.TextField()
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+# Answer Model
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    detail = models.TextField()
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
