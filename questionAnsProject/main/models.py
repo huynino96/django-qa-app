43bba551e60fb162fd74e7f26a94992ec78dec31
huynino96 <huynino0496@gmail.com>
2021-03-05 23:00:18 +0700

    Create Comment, Upvote, Downvote Model

--- questionAnsProject/main/models.py
+++ questionAnsProject/main/models.py
@@ -2,9 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+
 # Question Model
-
-
 class Question(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
@@ -14,9 +13,8 @@ class Question(models.Model):
     def __str__(self):
         return self.title
 
+
 # Answer Model
-
-
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     detail = models.TextField()
@@ -24,3 +22,25 @@ class Answer(models.Model):
 
     def __str__(self):
         return self.title
+
+
+# Comment Model
+class Comment(models.Model):
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comment_user')
+    create_time = models.DateTimeField(auto_now_add=True)
+
+
+# Upvoet Model
+class Upvote(models.Model):
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='upvote_user')
+
+
+# Downvote Model
+class Downvote(models.Model):
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='downvote_user')
